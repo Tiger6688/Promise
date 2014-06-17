dd1218736c47018c93797618db91b2ee701407da
Matt Galloway <mattjgalloway@fb.com>
2014-06-17 20:31:27 +0100

    Add basic API

--- Promise/Promise/Promises.swift
+++ Promise/Promise/Promises.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class Promise<I, O> {
+  
+  class func succeed(value: O) -> Promise<I, O> {
+    
+  }
+  
+  class func fail(error: String) -> Promise<I, O> {
+    
+  }
+  
+  typealias SuccessClosure = (value: O) -> Promise
+  typealias FailClosure = (error: String) -> Promise
+  
+  func then(succeed: SuccessClosure, fail: FailClosure) -> Promise<I, O> {
+    
+  }
+  
+}
